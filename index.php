b6297e9e35d2221b6fb53b677181de09ce066d46
aragas07 <argie.ragas@dorsu.edu.ph>
2022-10-24 14:09:55 +0800

    20924102022

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
+echo "Example";
 /*
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
